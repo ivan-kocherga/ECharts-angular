e8e8d0e21bdedafafddec67b697804c6a7c1fc5f
xolab_mac <xolab@mail.ru>
2021-08-02 15:56:29 +0300

    Выполнил задание

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxGaugeModule } from 'ngx-gauge';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndicatorComponent } from './components/indicator/indicator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndicatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxGaugeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
